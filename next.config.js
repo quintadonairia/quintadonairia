1ace4270b9706268d937b0d7ee9b4518bf6ca8c9
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-02-20 15:19:27 +0000

    Test maintenance page redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    if (process.env.NODE_ENV === "production") {
+      return [
+        {
+          source: "/lodging",
+          destination: "/maintenance",
+          permanent: false,
+        },
+        {
+          source: "/experiences",
+          destination: "/maintenance",
+          permanent: false,
+        },
+        {
+          source: "/restaurant",
+          destination: "/maintenance",
+          permanent: false,
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
 };
