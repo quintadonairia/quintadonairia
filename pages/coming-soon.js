aa69a20fc18afa1c2d8f1f91d3d8fcfeda76ba4d
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-02-28 16:23:55 +0000

    Translate Coming Soon page

--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -1,25 +1,40 @@
+import { useRouter } from "next/router";
+import en from "../locales/en/coming-soon.json";
+import pt from "../locales/pt/coming-soon.json";
+
 import { RiFacebookCircleFill, RiInstagramLine } from "react-icons/ri";
 
-const Maintenance = () => {
+const ComingSoon = () => {
+  const router = useRouter();
+  const { locale } = router;
+
+  let t;
+  switch (locale) {
+    case "en":
+      t = en;
+      break;
+    case "pt":
+      t = pt;
+      break;
+  }
+
   return (
     <div className="h-screen max-w-xs lg:max-w-xl m-auto flex flex-col items-center justify-between gap-16 py-24 text-center">
       <header>
         <img
-          src="./graphics/lockup-primary.svg"
+          src="../graphics/lockup-primary.svg"
           alt=""
           className="w-24 lg:w-32"
         />
       </header>
       <div className="flex flex-col items-center gap-3">
-        <h1 className="text-5xl lg:text-7xl font-serif">Coming Soon</h1>
-        <p className="text-lg lg:text-xl">
-          We are working on the website and drinking some 🍷
-        </p>
+        <h1 className="text-5xl lg:text-7xl font-serif">{t.heroHeading}</h1>
+        <p className="text-lg lg:text-xl">{t.heroDescription}</p>
       </div>
       <footer className="flex flex-col items-center justify-center gap-4">
         <div className="flex flex-col items-center gap-2">
-          <h2 className="font-serif text-2xl">Meanwhile</h2>
-          <p>You can check our social media and get to know us better</p>
+          <h2 className="font-serif text-2xl">{t.socialHeading}</h2>
+          <p>{t.socialDescription}</p>
         </div>
         <div className="flex gap-4">
           <a href="https://www.facebook.com/quintadonairia" target="_blank">
@@ -34,4 +49,4 @@ const Maintenance = () => {
   );
 };
 
-export default Maintenance;
+export default ComingSoon;
