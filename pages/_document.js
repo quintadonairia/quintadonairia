f374a62c0ce0574a6e93a9b3db91d6c64028f4c0
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-04-03 15:56:07 +0100

    Add favicons for all platforms

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,29 @@ class MyDocument extends Document {
             name="description"
             content="A wellness haven, combining wine and olive oil with the rich Portuguese cuisine"
           ></meta>
+          <meta name="theme-color" content="#fcfaf8"></meta>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Playfair+Display:wght@400;600&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,6 @@ const Home = () => {
     <div className="flex flex-col items-center min-h-screen relative overflow-x-hidden">
       <Head>
         <title>Quinta Dona Iria - Enoturismo | Home</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Hero Section */}
