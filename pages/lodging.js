29eba71eb049564a1112b53baac735e7b3f309f9
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-04-10 22:04:09 +0100

    Refactor Lodging page

--- pages/lodging.js
+++ pages/lodging.js
@@ -1,17 +1,14 @@
-import Button from "../components/atoms/Button";
-import Icon from "../components/atoms/Icon";
-import LayoutPrimary from "../components/layouts/LayoutPrimary";
 import LayoutSplit from "../components/layouts/LayoutSplit";
 import Room from "../components/molecules/Room";
 import Hero from "../components/organisms/Hero";
-
-import { RiArrowRightLine } from "react-icons/ri";
+import Container from "../components/organisms/Container";
+import Block from "../components/molecules/Block";
 
 const Lodging = () => {
   return (
     <div className="flex flex-col min-h-screen relative overflow-x-hidden">
       <Hero title="Lodging" image="url(images/20210820-donairia-008.jpg" />
-      <LayoutPrimary>
+      <Container>
         <div className="flex flex-col gap-4">
           <h1 className="text-4xl font-serif text-olive-500">
             Each room, a special person
@@ -38,34 +35,20 @@ const Lodging = () => {
             description="2 persons, approx. 25m² from 75€"
           />
         </div>
-      </LayoutPrimary>
-      <LayoutSplit
-        childrenLeading={
-          <>
-            <img
-              src="/images/senjuti-kundu-GCI8dqi4uWM-unsplash.jpg"
-              alt=""
-              className="w-full lg:w-[28rem] aspect-[5/4] lg:aspect-[4/5] object-cover object-[center,65%]"
-            />
-          </>
-        }
-        childrenTrailing={
-          <>
-            <h1 className="font-serif text-2xl lg:text-4xl">Family Friendly</h1>
-            <div className="flex flex-col gap-4 text-sm lg:text-base">
-              <p className="leading-relaxed">
-                Making it a family affair? Get in touch to find out about our
+      </Container>
+      <LayoutSplit vDirection="normal">
+        <img
+          src="/images/senjuti-kundu-GCI8dqi4uWM-unsplash.jpg"
+          alt=""
+          className="w-full lg:w-[36rem] aspect-[5/4] lg:aspect-[5/4] object-cover"
+        />
+        <Block
+          title="Family Friendly"
+          text="Making it a family affair? Get in touch to find out about our
                 family friendly room styles and options for when you're bringing
-                the children along.
-              </p>
-              <p className="leading-relaxed">
-                Of course, we also welcome our four-legged friends in certain
-                specific rooms, at a charge of €10 per night.
-              </p>
-            </div>
-          </>
-        }
-      />
+                the children along."
+        />
+      </LayoutSplit>
     </div>
   );
 };
