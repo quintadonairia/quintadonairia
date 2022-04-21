ccef5ff61ade1b1910342e58cbb3ed062905ba3a
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-04-21 22:45:53 +0100

    Remove redirects to Coming Soon page

--- next.config.js
+++ next.config.js
@@ -4,22 +4,4 @@ module.exports = {
     defaultLocale: "en",
   },
   reactStrictMode: true,
-  async redirects() {
-    if (process.env.NODE_ENV === "production") {
-      return [
-        {
-          source: "/lodging",
-          destination: "/coming-soon",
-          permanent: false,
-        },
-        {
-          source: "/restaurant",
-          destination: "/coming-soon",
-          permanent: false,
-        },
-      ];
-    } else {
-      return [];
-    }
-  },
 };
