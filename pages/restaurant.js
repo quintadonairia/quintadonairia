b04416e5134b30b28d4abd89fb8a6f465a3db278
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-04-30 15:25:42 +0100

    Add Header section to restaurant page

--- pages/restaurant.js
+++ pages/restaurant.js
@@ -4,6 +4,8 @@ import en from "../locales/en/restaurant.json";
 import pt from "../locales/pt/restaurant.json";
 
 import Hero from "../components/organisms/Hero";
+import Header from "../components/molecules/Header";
+import Container from "../components/layouts/Container";
 
 const Restaurant = () => {
   const router = useRouter();
@@ -26,8 +28,26 @@ const Restaurant = () => {
       </Head>
       <Hero
         title={t.heroTitle}
-        image="url(/images/20211003-donairia-050.jpg)"
+        image="url(/images/20220423-donairia-004.jpg)"
       />
+
+      {/* Reservation Form */}
+      <section>
+        <Container>
+          <div className="flex flex-col gap-16 items-center">
+            <img
+              src="/graphics/wordmark-dark.svg"
+              alt=""
+              className="w-48 lg:w-64"
+            />
+            <Header heading={t.headerHeading} subheading={t.headerSubheading} />
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </div>
+        </Container>
+      </section>
     </div>
   );
 };
