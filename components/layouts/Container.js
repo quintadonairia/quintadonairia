2f9a95d5f237c8eb33cb52c8756bb45adc6ad4c6
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-05-22 18:55:11 +0100

    fix(components): fix uneven margins on Container component

--- components/layouts/Container.js
+++ components/layouts/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="px-6 py-12 lg:px-12 lg:py-16 max-w-screen-xl">
+    <div className="max-w-screen-xl m-auto px-6 py-12 lg:px-12 lg:py-16">
       {children}
     </div>
   );
