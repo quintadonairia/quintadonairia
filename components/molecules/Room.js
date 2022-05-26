8b328c5525aa651d20d9cf5a0b6a0b8882c8c0e0
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-05-26 16:03:49 +0100

    feat(components): add price prop to Room.js component

--- components/molecules/Room.js
+++ components/molecules/Room.js
@@ -3,7 +3,14 @@ import Icon from "../atoms/Icon";
 
 import { RiArrowRightLine } from "react-icons/ri";
 
-const Room2 = ({ imageSrc, imageAlt, title, description, buttonLabel }) => {
+const Room2 = ({
+  imageSrc,
+  imageAlt,
+  title,
+  price,
+  description,
+  buttonLabel,
+}) => {
   return (
     <article className="flex flex-col gap-8 items-center">
       <img
@@ -13,6 +20,9 @@ const Room2 = ({ imageSrc, imageAlt, title, description, buttonLabel }) => {
       />
       <div className="flex flex-col gap-4 items-center text-center lg:px-16">
         <h1 className="font-serif text-3xl text-olive-500">{title}</h1>
+        <h3 className="bg-olive-500 text-tan-50 py-2 px-4 rounded-full uppercase text-xs tracking-widest">
+          {price}
+        </h3>
         <h4 className="text-neutral-600 leading-relaxed">{description}</h4>
         <a href="mailto:hello@quintadonairia.com?subject=Pedido de Reserva&body=Olá!%0A%0AObrigado por escolher a Quinta Dona Iria.%0A%0AInformações que precisamos para a reserva:%0A%0A- Dias da estadia%0A- Nº de pessoas%0A- Nome da reserva%0A- Quarto pretendido (Quarto Vista Olival ou Quarto Vista Serra)">
           <Button size="lg" variant="link" mode="light">
