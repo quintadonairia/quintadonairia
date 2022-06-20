0ca295997dc8c67dbb5e6f435929484f61f489f4
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-06-20 11:46:55 +0100

    feat(routes): add exception to app Layout on links page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import Layout from "../components/layouts/Layout";
 
+import { useRouter } from "next/router";
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  const router = useRouter();
+  const { pathname } = router;
+
+  if (pathname === "/links") {
+    return <Component {...pageProps} />;
+  } else {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
 
 export default MyApp;
