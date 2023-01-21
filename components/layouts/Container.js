2f7e8646e77504c3b8820997bdd1765d23f26433
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2023-01-21 12:52:26 +0000

    :art: Remove unneeded React imports

--- components/layouts/Container.js
+++ components/layouts/Container.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Container = ({ children }) => {
   return (
     <div className="max-w-screen-xl m-auto px-6 py-12 lg:px-12 lg:py-16">
--- components/molecules/Block.js
+++ components/molecules/Block.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Button from "../atoms/Button";
 
 const Block = ({ title, text, action, label }) => {
