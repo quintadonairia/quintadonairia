8a3379dc214083661cd183e251f8fd58b3ba3d13
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-04-22 10:29:52 +0100

    :fire: Remove unused Home route

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,12 @@
-import { useRouter } from "next/router";
 import Layout from "../components/layouts/Layout";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const { pathname } = router;
-
-  if (pathname === "/home") {
-    return <Component {...pageProps} />;
-  } else {
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
-  }
+function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp;
+export default App;
