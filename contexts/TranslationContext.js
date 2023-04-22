7c8a32904e6501b61938b9a1684ef39e01d77689
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2023-04-22 19:37:19 +0100

    :sparkles: Add translation context functions

---
+++ contexts/TranslationContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+import en from "../locales/en.json";
+import pt from "../locales/pt.json";
+
+const TranslationContext = createContext();
+
+export const TranslationProvider = ({ children, locale }) => {
+  const t = locale === "pt" ? pt : en;
+
+  return (
+    <TranslationContext.Provider value={t}>
+      {children}
+    </TranslationContext.Provider>
+  );
+};
+
+export const useTranslation = () => {
+  const t = useContext(TranslationContext);
+
+  return t;
+};
