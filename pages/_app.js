8ae2ebff2b82ab4fa51625abe11bb576a75e2d63
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-04-22 11:39:10 +0100

    :art: Uniformize export function statements

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import Layout from "../components/layouts/Layout";
 import "../styles/globals.css";
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
-
-export default App;
