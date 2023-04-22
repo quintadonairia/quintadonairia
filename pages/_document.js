1620b39a90e75a583eef17bc904491a8637b8a42
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-04-22 11:39:10 +0100

    :art: Uniformize export function statements

--- pages/_document.js
+++ pages/_document.js
@@ -1,45 +1,41 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html className="scroll-smooth">
-        <Head>
-          <meta
-            name="description"
-            content="A wellness haven, combining wine and olive oil with the rich Portuguese cuisine"
-          ></meta>
-          <meta name="theme-color" content="#fcfaf8"></meta>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Playfair+Display:wght@400;600&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="description"
+          content="A wellness haven, combining wine and olive oil with the rich Portuguese cuisine"
+        />
+        <meta name="theme-color" content="#fcfaf8" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Playfair+Display:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
