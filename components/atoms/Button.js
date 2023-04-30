aadc01a84211b6d1c30e62d32d4b5a33ac45e870
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-04-30 17:19:29 +0100

    :sparkles: Add condition to render anchor tag

--- components/atoms/Button.js
+++ components/atoms/Button.js
@@ -1,34 +1,56 @@
-const Button = ({ children, size, variant, mode, desktop, mobile }) => {
-  const sizes = {
-    sm: "text-xs px-6 py-3",
-    md: "text-xs px-8 py-4",
-    lg: "text-sm px-10 py-4",
-  };
+import Link from "next/link";
+import { forwardRef } from "react";
 
-  const variants = {
-    primary:
-      "bg-transparent border-2 text-olive border-olive hover:bg-olive hover:text-white duration-300 ",
-    secondary:
-      "bg-transparent border-2 text-white border-white hover:bg-white hover:text-black duration-300 ",
-    link: "bg-transparent hover:gap-2 duration-200",
-  };
+const Button = forwardRef(
+  ({ children, href, size, variant, mode, desktop, mobile }, ref) => {
+    const sizes = {
+      sm: "px-6 py-3 text-xs",
+      md: "px-8 py-4 text-xs",
+      lg: "px-10 py-4 text-sm",
+    };
 
-  const modes = {
-    light: "text-black border-black",
-    dark: "text-white border-white",
-  };
+    const variants = {
+      primary:
+        "bg-transparent border-2 text-olive border-olive hover:bg-olive hover:text-white duration-300 ",
+      secondary:
+        "bg-transparent border-2 text-white border-white hover:bg-white hover:text-black duration-300 ",
+      link: "bg-transparent hover:gap-2 duration-200",
+    };
 
-  return (
-    <button
-      className={`flex w-fit cursor-pointer flex-row items-center gap-1 font-bold uppercase tracking-widest ${
-        variants[variant]
-      } ${sizes[size]} ${modes[mode]} ${desktop && "hidden lg:block"} ${
-        mobile && "block lg:hidden"
-      }`}
-    >
-      {children}
-    </button>
-  );
-};
+    const modes = {
+      light: "text-black border-black",
+      dark: "text-white border-white",
+    };
+
+    if (href) {
+      return (
+        <Link href={href} passHref>
+          <a
+            className={`flex w-fit cursor-pointer flex-row items-center gap-1 font-bold uppercase tracking-widest ${
+              variants[variant]
+            } ${sizes[size]} ${modes[mode]} ${desktop && "hidden lg:block"} ${
+              mobile && "block lg:hidden"
+            }`}
+            ref={ref}
+          >
+            {children}
+          </a>
+        </Link>
+      );
+    }
+
+    return (
+      <button
+        className={`flex w-fit cursor-pointer flex-row items-center gap-1 font-bold uppercase tracking-widest ${
+          variants[variant]
+        } ${sizes[size]} ${modes[mode]} ${desktop && "hidden lg:block"} ${
+          mobile && "block lg:hidden"
+        }`}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default Button;
