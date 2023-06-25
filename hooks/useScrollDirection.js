f5b05affeb61de3bccbb265662683fbea6b09b2a
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-06-25 19:36:04 +0100

    :sparkles: Add scroll direction hook

---
+++ hooks/useScrollDirection.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useScrollDirection = () => {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.scrollY;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+
+      if (
+        direction !== scrollDirection &&
+        (scrollY > lastScrollY || scrollY < lastScrollY)
+      ) {
+        setScrollDirection(direction);
+      }
+
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+
+    window.addEventListener("scroll", updateScrollDirection);
+
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection);
+    };
+  }, [scrollDirection]);
+
+  return scrollDirection;
+};
+
+export default useScrollDirection;
