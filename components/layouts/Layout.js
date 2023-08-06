1c52ac2e9a4d08a1cb8e68c1695bbdec66a8371c
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2023-08-06 16:20:09 +0100

    :sparkles: Add Layout state to handle Menu and Header interactivity

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
+import Menu from "../organisms/Menu";
 
 export default function Layout({ children }) {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleMenuIsActive = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
   return (
     <div className="flex flex-col bg-light">
-      <Header />
+      <Header
+        handleMenuIsActive={handleMenuIsActive}
+        isMenuActive={isMenuActive}
+      />
+      <Menu
+        handleMenuIsActive={handleMenuIsActive}
+        isMenuActive={isMenuActive}
+      />
       <main>{children}</main>
       <Footer />
     </div>
