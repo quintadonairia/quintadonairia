7666e5b428768eebcf8cdd317dfbb27331bb52d1
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-08-11 14:18:39 +0100

    :art: Improve Button styles structure

--- components/atoms/Button.js
+++ components/atoms/Button.js
@@ -1,21 +1,22 @@
 import Link from "next/link";
 import { forwardRef } from "react";
+const styles = {
+  base: "inline-flex w-fit cursor-pointer items-center gap-1 font-bold uppercase tracking-widest duration-200 hover:gap-2",
+  size: {
+    small: "px-6 py-3 text-sm",
+    medium: "px-8 py-4 text-sm",
+    large: "px-10 py-5 text-sm",
+  },
+  variant: {
+    brand:
+      "border-2 text-olive border-olive hover:bg-olive hover:text-white duration-300",
+    neutral:
+      "border-2 text-white border-white hover:bg-white hover:text-black duration-300",
+  },
+};
 
 const Button = forwardRef(
   ({ children, className, href, size, variant }, ref) => {
-    const sizes = {
-      sm: "px-6 py-3 text-xs",
-      md: "px-8 py-4 text-xs",
-      lg: "px-10 py-4 text-sm",
-    };
-
-    const variants = {
-      primary:
-        "border-2 text-olive border-olive hover:bg-olive hover:text-white duration-300 ",
-      secondary:
-        "border-2 text-white border-white hover:bg-white hover:text-black duration-300 ",
-    };
-
     if (href) {
       return (
         <Link href={href} passHref>
