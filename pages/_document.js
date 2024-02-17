e503a84b5baa361a97a8f27b8ecb17a3b05e53a8
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-02-17 20:22:03 +0000

    :sparkles: Update global site document with new assets

--- pages/_document.js
+++ pages/_document.js
@@ -8,29 +8,19 @@ export default function Document() {
           name="description"
           content="A wellness haven, combining wine and olive oil with the rich Portuguese cuisine"
         />
-        <meta name="theme-color" content="#fcfaf8" />
+        <meta name="theme-color" content="#faf6f0" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Playfair+Display:wght@400;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
       <body>
         <Main />
