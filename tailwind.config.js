8399eff21c12dcfb24041e206f9c1d7ebe38dfb2
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-04-10 22:24:19 +0100

    :sparkles: Add color system

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,27 @@ module.exports = {
         serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
       },
       colors: {
+        background: {
+          default: "hsl(var(--color-background-default))",
+          inverse: "hsl(var(--color-background-inverse))",
+          strong: "hsl(var(--color-background-strong))",
+          subtle: "hsl(var(--color-background-subtle))",
+          brand: "hsl(var(--color-background-brand))",
+        },
+        foreground: {
+          default: "hsl(var(--color-foreground-default))",
+          inverse: "hsl(var(--color-foreground-inverse))",
+          strong: "hsl(var(--color-foreground-strong))",
+          subtle: "hsl(var(--color-foreground-subtle))",
+          brand: "hsl(var(--color-foreground-brand))",
+        },
+        border: {
+          default: "hsl(var(--color-border-default))",
+          inverse: "hsl(var(--color-border-inverse))",
+          strong: "hsl(var(--color-border-strong))",
+          subtle: "hsl(var(--color-border-subtle))",
+          brand: "hsl(var(--color-border-brand))",
+        },
         light: "#FCFAF8",
         dark: "#151414",
         olive: "#6E6E0C",
