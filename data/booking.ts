148b67406e236c5c3716109a13f9e8bd101aa5d5
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-04-13 23:19:42 +0100

    🛠️  Move booking steps data to separate data file

---
+++ data/booking.ts
@@ -0,0 +1,26 @@
+export const booking = [
+  {
+    name: "Dates",
+    slug: "dates",
+  },
+  {
+    name: "Guests",
+    slug: "guests",
+  },
+  {
+    name: "Rooms",
+    slug: "rooms",
+  },
+  {
+    name: "Name",
+    slug: "name",
+  },
+  {
+    name: "Email",
+    slug: "email",
+  },
+  {
+    name: "Confirmation",
+    slug: "confirmation",
+  },
+];
