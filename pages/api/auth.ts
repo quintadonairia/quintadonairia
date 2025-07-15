bbbf0d34adb66abab589251e89c47443868b6342
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-07-15 23:41:34 +0100

    🛠️ Decode auth credentials using base64

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,13 +1,17 @@
 import { google } from "googleapis";
 
 export default async function GoogleSheetsAuth() {
-  const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  const credentials = JSON.parse(
+    Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS, "base64").toString(
+      "utf-8"
+    )
+  );
 
   const auth = await google.auth.getClient({
     projectId: credentials.project_id,
     credentials: {
       type: "service_account",
-      private_key: credentials.private_key.replace(/\\n/g, "\n"),
+      private_key: credentials.private_key,
       client_email: credentials.client_email,
       client_id: credentials.client_id,
       token_url: credentials.token_uri,
